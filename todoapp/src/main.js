4d9f5b0dd8808283c7d1dc9eec14fb83813e54d8
LeaderLou1 <louis28jager@gmail.com>
2023-12-19 21:20:46 -0500

    Your commit message

---
+++ todoapp/src/main.js
@@ -0,0 +1,74 @@
+import './style.css'
+import { v4 as uuidv4 } from 'uuid';
+import { getAllTodos, initializeTodosIfEmpty, addTodo, toggleTodoComplete, deleteTodo } from './data-layer-utils';
+
+const renderTodoCard = (todo) => {
+  const todosList = document.querySelector("ul#todos-list");
+  const li = document.createElement('li');
+  const h3 = document.createElement('h3');
+
+  li.dataset.uuid = todo.uuid;
+  li.classList.add('todo-card');
+  h3.textContent = todo.title;
+
+  const labelInputButton = document.createElement('div');
+  labelInputButton.innerHTML = 
+  `
+    <div class='label-input-container'>
+      <label>Complete</label>
+      <input type="checkbox" name="isComplete" ${todo.isComplete ? "checked" : ""}>
+    </div>
+    <button class='delete-todo'>🗑️</button>`;
+    li.append(h3, labelInputButton);
+    todosList.append(li);
+};
+
+const renderTodos = () => {
+ document.querySelector("ul#todos-list").innerHTML = "";
+ getAllTodos().forEach(renderTodoCard);
+};
+
+const handleTodoChange = (e) => {
+  if (!e.target.matches('input[type="checkbox"]')) return;
+  const uuid = e.target.closest('#todos-list>li').dataset.uuid;
+  toggleTodoComplete(uuid);
+  renderTodos();
+}
+
+const handleDeleteTodo = (e) => {
+  if(!e.target.matches('button.delete-todo')) return;
+  const uuid = e.target.closest('#todos-list>li').dataset.uuid;
+  deleteTodo(uuid);
+  renderTodos();
+}
+
+const handleNewTodo = (e) => {
+  e.preventDefault();
+
+  const form = e.target;
+  const newTodo = {
+    uuid: uuidv4(),
+    title: form.todoTitle.value,
+    isComplete: false
+  };
+  addTodo(newTodo);
+  renderTodos();
+
+  form.reset();
+};
+
+const main = () => {
+  initializeTodosIfEmpty();
+  renderTodos();
+
+  document.querySelector("ul#todos-list").addEventListener('input', handleTodoChange);
+  document.querySelector("ul#todos-list").addEventListener('click', handleDeleteTodo);
+  document.querySelector("form#new-todo-form").addEventListener('submit', handleNewTodo);
+};
+
+
+main();
+
+
+
+
